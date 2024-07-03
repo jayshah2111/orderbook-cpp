25fe744af6f5fa5cb063b43620032c3636294fbe
Jay <shahjay2111@gmail.com>
2024-07-03 13:13:00 +0530

    Created Orderbook class and defined OrderEntry

--- orderbook.h
+++ orderbook.h
@@ -5,3 +5,19 @@
 #include <thread>
 #include <condition_variable>
 #include <mutex>
+
+#include "Usings.h"
+#include "Order.h"
+#include "OrderModify.h"
+#include "OrderbookLevelInfos.h"
+#include "Trade.h"
+
+class Orderbook
+{
+private:
+    struct OrderEntry
+    {
+        OrderPointer order_{ nullptr };
+        OrderPointers::iterator location_;
+    };
+};
