e7aa96b703dd2065554d211508995eb4a6db4d0b
Jay <shahjay2111@gmail.com>
2024-07-05 13:46:00 +0530

    Defined enums for test file

--- OrderbookTest/test.cpp
+++ OrderbookTest/test.cpp
@@ -5,3 +5,24 @@
 #include <filesystem>
 #include <iostream>
 #include <fstream>
+
+#include "../Orderbook.cpp"
+
+namespace googletest = ::testing;
+
+enum class ActionType
+{
+    Add,
+    Cancel,
+    Modify,
+};
+
+struct Information
+{
+    ActionType type_;
+    OrderType orderType_;
+    Side side_;
+    Price price_;
+    Quantity quantity_;
+    OrderId orderId_;
+};
